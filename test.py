bc461040c5c9577e8d905cf2515e840eb34d912a
tanjir053 <119875071+tanjir053@users.noreply.github.com>
2022-12-05 15:43:11 +0000

    created a function

--- test.py
+++ test.py
@@ -1,1 +1,8 @@
-print('hello motherfucker python')
+def greetings(name, age):
+    return f"hello dear {name}, welcome to tanjir tech \n You are {age} years old"
+
+
+print(greetings('sarwar', 27))
+print(greetings('tanjir', 25))
+
+
